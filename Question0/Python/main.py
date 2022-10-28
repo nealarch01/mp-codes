aac7672ea4da69458c1925d3108568ae57849227
Neal A <73256760+nealarch01@users.noreply.github.com>
2022-10-28 14:35:31 -0700

    Fix Python code for clarity

--- Question0/Python/main.py
+++ Question0/Python/main.py
@@ -4,8 +4,8 @@ def productOfArrayExceptSelf(nums) -> list[int]:
     right = [1] * n # If n == 3, this creates an array [1, 1, 1]
     for i in range(1, n):
         left[i] = left[i - 1] * nums[i - 1]
-    for i in range(n-2, -1, -1): # start at n-2, end at 0, decrement -1
-        right[i] = right[i+1] * nums[i+1]
+    for i in range(n - 2, -1, -1): # start at n-2, end at 0 (-1), decrement -1
+        right[i] = right[i + 1] * nums[i + 1]
     result = []
     for i in range(n):
         result.append(left[i] * right[i])
@@ -14,7 +14,7 @@ def productOfArrayExceptSelf(nums) -> list[int]:
 
 
 def main():
-    result = productOfArrayExceptSelf([1,2,3,4])
+    result = productOfArrayExceptSelf([1, 2, 3, 4, 5])
     print(result)
 
 if __name__ == "__main__":
